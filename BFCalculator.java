14f3d6e0f01e5663bdd2c1a9971b87d09285595e
rrrr4444 <reedcolloton@gmail.com>
2023-09-13 21:03:03 -0500

    save

--- BFCalculator.java
+++ BFCalculator.java
@@ -5,10 +5,34 @@ public class BFCalculator {
     BigFraction result;
 
     public BigFraction evaluate(String exp) {
-        // remove all spaces;
-        exp.replaceAll(" ", "");
-
-        return new BigFraction(0, 0);
+        String[] elements = exp.split(" ");
+        BigFraction[] values = new BigFraction[exp.length()];
+        // Separate operations from values
+        // If numerical string, convert to BigFraction
+        for (int i = 0; i < elements.length; i++) {
+            // If fraction, convert to fraction
+            if (elements[i].contains("/")
+                    && elements[i].length() != 1
+                    || is_numeric(elements[i])) {
+                values[i] = new BigFraction(elements[i]);
+            } // if
+            // If register value, retrieve
+            else if (Character.isAlphabetic(elements[i].charAt(0))) {
+                Character c = elements[i].charAt(0);
+                int register_index = (int) c - (int) 'a';
+                values[i] = registers[register_index];
+            } // else if
+        } // for
+        // set total to first value
+        BigFraction total = values[0];
+        Boolean operation = true;
+        for (int i = 1; i < elements.length; i = i + 2) {
+            if (operation) {
+                ;
+            }
+            operation = !operation;
+        } // for
+        return total;
     } // evaluate()
 
     // Stores the last result in named register a-z
@@ -16,5 +40,14 @@ public class BFCalculator {
         int index = (int) register - (int) 'a';
         registers[index] = this.result;
     } // store(char register)
+
+    Boolean is_numeric(String element) {
+        for (int i = 0; i < element.length(); i++) {
+            if (!Character.isDigit(element.charAt(i))) {
+                return false;
+            } // if
+        } // for
+        return true;
+    } // is_numeric
 
 } // class BFCalculator
