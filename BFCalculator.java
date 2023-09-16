26e049885c1cc8aac4993f8403375907f5520a6e
rrrr4444 <reedcolloton@gmail.com>
2023-09-16 15:55:33 -0500

    comments and some small refactors

--- BFCalculator.java
+++ BFCalculator.java
@@ -1,5 +1,5 @@
 /**
- * A calculator for Fractions.
+ * A simple calculator for Fractions.
  * Used by InteractiveCalculator and QuickCalculator.
  *
  * @author Reed Colloton
@@ -10,13 +10,14 @@ public class BFCalculator {
     // Saves the last computation for the STORE command
     BigFraction result = null;
 
+    // Evaluates an expression for the calculator
     public BigFraction evaluate(String exp) throws Exception {
         String[] elements = exp.split(" ");
         if (elements.length % 2 != 1) {
             throw new Exception("Invalid arguments");
-        }
+        } // if
         // set total to first value
-        BigFraction total = parseNumber(elements[0]);
+        BigFraction total = parseValue(elements[0]);
         String operation = "";
         for (int i = 1; i < elements.length; i++) {
             if (i % 2 == 1) { // Odd elements are operations
@@ -27,15 +28,15 @@ public class BFCalculator {
                 }
             } else { // Even elements are values
                 if (operation.equals("/")) {
-                    total = total.divide(parseNumber(elements[i]));
+                    total = total.divide(parseValue(elements[i]));
                 } else if (operation.equals("*")) {
-                    total = total.multiply(parseNumber(elements[i]));
+                    total = total.multiply(parseValue(elements[i]));
                 } else if (operation.equals("+")) {
-                    total = total.add(parseNumber(elements[i]));
+                    total = total.add(parseValue(elements[i]));
                 } else if (operation.equals("-")) {
-                    total = total.subtract(parseNumber(elements[i]));
+                    total = total.subtract(parseValue(elements[i]));
                 }
-            }
+            } // if
         } // for
         // Save last result for store method
         this.result = total;
@@ -44,20 +45,34 @@ public class BFCalculator {
 
     // Stores the last result in named register a-z
     public void store(char register) throws Exception {
-        int index = (int) register - (int) 'a';
+        int i = registerIndex(register);
         if (this.result == null) {
             throw new Exception("No value to store");
         } else {
-            this.registers[index] = this.result;
+            this.registers[i] = this.result;
         }
     } // store(char register)
 
+    // Return BigFraction from string representation or register letter
+    BigFraction parseValue(String num) throws Exception {
+        // Throws exceptions if value is invalid
+        validateValue(num);
+        // If register value, retrieve
+        char c = num.charAt(0);
+        if (Character.isAlphabetic(c)) {
+            int i = registerIndex(c);
+            return registers[i];
+        } else {
+            // Else number, convert to fraction
+            return new BigFraction(num);
+        } // else
+    } // parseValue()
+
+    // Validates a value for the calculator, raises appropriate errors if invalid
     void validateValue(String num) throws Exception {
-        if (num.length() == 1) {
-            int i = (int) num.charAt(0) - (int) 'a';
-            if (i >= 26 || i < 0) {
-                throw new Exception("Invalid register value");
-            } else if (this.registers[i] == null) {
+        if (num.length() == 1 && !Character.isDigit(num.charAt(0))) {
+            int i = registerIndex(num.charAt(0));
+            if (this.registers[i] == null) {
                 throw new Exception("Empty register");
             }
         } else {
@@ -74,20 +89,14 @@ public class BFCalculator {
                 throw new Exception("Invalid fraction");
             }
         }
-    }
+    } // validateValue()
 
-    // Return BigFraction from string representation or register letter
-    BigFraction parseNumber(String num) throws Exception {
-        // Throws exceptions if invalid
-        validateValue(num);
-        // If register value, retrieve
-        char c = num.charAt(0);
-        if (Character.isAlphabetic(c)) {
-            int i = (int) c - (int) 'a';
-            return registers[i];
-        } else {
-            // Else number, convert to fraction
-            return new BigFraction(num);
-        } // else
-    } // parseNumber()
+    // Calculates an index value for the register given char c
+    int registerIndex(Character c) throws Exception {
+        int i = (int) c - (int) 'a';
+        if (i >= 26 || i < 0) {
+            throw new Exception("Invalid register value");
+        }
+        return i;
+    } // registerIndex()
 } // class BFCalculator
